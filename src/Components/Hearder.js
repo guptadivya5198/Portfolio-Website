4bb1953fec796eb0886351ae819c0113e5a643fa
guptadivya5198 <guptadivya5198@gmail.com>
2024-10-18 17:49:53 +0530

    hdfhd

--- src/Components/Hearder.js
+++ src/Components/Hearder.js
@@ -5,14 +5,14 @@ const Hearder = () => {
   return (
     <div className="w-full bg-gray-50">
       <nav className="h-[3.5rem] w-[100%] flex justify-between items-center sm:px-6 md:px-20 lg:px-28">
-        <div className="flex items-center w-[40%]">
-          <FaDochub className="text-3xl sm:text-xl" />
-          <p className="ml-0 mt-3 sm:text-lg md:text-3xl lg:text-2xl font-semibold">
+        <div className="flex items-center w-[38%]">
+          <FaDochub className="lg:text-4xl md:text-4xl sm:text-xl sm:ml-0" />
+          <p className="ml-0 sm:ml-0 lg:mt-3 sm:text-lg md:text-3xl lg:text-2xl font-semibold">
             IVYA GUPTA
           </p>
         </div>
-        <div className="w-[60%]">
-          <ul className="flex gap-x-4 sm:gap-x-5 lg:gap-x-8 sm:text-base md:text-2xl lg:text-2xl cursor-pointer">
+        <div className="w-[62%]">
+          <ul className="flex gap-x-6 lg:ml-[50%] md:ml-[30%] sm:gap-x-5 lg:gap-x-8 sm:text-xs sm:mt-1 md:text-2xl lg:text-2xl cursor-pointer">
             <li>
               <a href="#Project">Project</a>
             </li>
