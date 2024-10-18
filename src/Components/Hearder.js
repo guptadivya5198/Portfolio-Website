8b25e1502d0435fe29355afd0d5294be4e2cc24f
guptadivya5198 <guptadivya5198@gmail.com>
2024-10-18 17:38:32 +0530

    dh

--- src/Components/Hearder.js
+++ src/Components/Hearder.js
@@ -4,15 +4,15 @@ import { FaDochub } from 'react-icons/fa6';
 const Hearder = () => {
   return (
     <div className="w-full bg-gray-50">
-      <nav className="h-[3.5rem] w-[100%] flex justify-between items-center px-6 sm:px-10 lg:px-28">
+      <nav className="h-[3.5rem] w-[100%] flex justify-between items-center sm:px-6 md:px-20 lg:px-28">
         <div className="flex items-center w-[40%]">
-          <FaDochub className="text-3xl sm:text-3xl" />
+          <FaDochub className="text-3xl sm:text-xl" />
           <p className="ml-0 mt-3 sm:text-lg md:text-3xl lg:text-2xl font-semibold">
             IVYA GUPTA
           </p>
         </div>
         <div className="w-[60%]">
-          <ul className="flex gap-x-4 sm:gap-x-6 lg:gap-x-8 text-lg sm:text-lg md:text-2xl lg:text-2xl cursor-pointer">
+          <ul className="flex gap-x-4 sm:gap-x-5 lg:gap-x-8 sm:text-base md:text-2xl lg:text-2xl cursor-pointer">
             <li>
               <a href="#Project">Project</a>
             </li>
