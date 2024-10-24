4236bc16afb842655b5b4ab1a8131f862f7a7c40
guptadivya5198 <guptadivya5198@gmail.com>
2024-10-24 17:43:02 +0530

    img

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -12,9 +12,9 @@ import SendIcon from '@mui/icons-material/Send';
 const HomePage = () => {
   return (
     <section id="Home">
-      <div className="flex border border-red-600 flex-col lg:flex-row md:flex-row relative lg:w-[1200px] lg:h-[490px] md:w-[800px] md:h-[240px] sm:w-[300px] sm:h-[300px] items-center justify-center  md:gap-24 lg:my-6 sm:my-12 md:my-12 lg:mx-auto md:mx-auto lg:py-4 lg:px-7 sm:px-6 md:px-6">
+      <div className="flex flex-col lg:flex-row md:flex-row relative lg:w-[1200px] lg:h-[490px] md:w-[800px] md:h-[240px] sm:w-[300px] sm:h-[300px] items-center justify-center  md:gap-24 lg:my-6 sm:my-12 md:my-12 lg:mx-auto md:mx-auto lg:py-4 lg:px-7 sm:px-6 md:px-6">
         {/* left Section  {Text & button}*/}
-        <div className="flex border border-red-600 flex-col justify-center w-full h-[50%] lg:w-[65%] lg:h-[90%] md:w-[90%] md:h-[93%] sm:w-full sm:h-[50%] lg:gap-6 md:gap-0 sm:gap-8 lg:mt-1 md:mt-2 text-center md:text-left">
+        <div className="flex flex-col justify-center w-full h-[50%] lg:w-[65%] lg:h-[90%] md:w-[90%] md:h-[93%] sm:w-full sm:h-[50%] lg:gap-6 md:gap-0 sm:gap-8 lg:mt-1 md:mt-2 text-center md:text-left">
           <div className="lg:text-2xl md:text-lg sm:text-3xl font-normal">
             Hi!! Divya Gupta
           </div>
@@ -50,7 +50,7 @@ const HomePage = () => {
           </div>
           <div className="flex lg:mt-[1%] md:mt-0 sm:mt-10 lg:mr-[82%] md:items-center md:justify-center sm:items-center">
             <Button
-              className="bg-red-500 mx-auto w-[26%] h-9 lg:w-[100%] lg:h-[90%] md:[40%] sm:w-[26%] sm:h-[90%] xs:w-[30%] xs:h-[50%] lg:px-2 md:px-2 lg:py-2 md:py-2 py-1 mt-0 text-base !shadow-red-200 font-semibold cursor-pointer transition-all duration-75 border-3 border-transparent hover:scale-105"
+              className="bg-red-500 mx-auto w-[30%] h-9 lg:w-[100%] lg:h-[90%] md:[40%] sm:w-[26%] sm:h-[90%] xs:w-[30%] xs:h-[50%] lg:px-2 md:px-2 lg:py-2 md:py-2 py-1 mt-0 text-base !shadow-red-200 font-semibold cursor-pointer transition-all duration-75 border-3 border-transparent hover:scale-105"
               variant="contained"
               SendIcon={<SendIcon />}
               onClick={() =>
@@ -65,19 +65,10 @@ const HomePage = () => {
           </div>
         </div>
         {/* Right Section */}
-        <div className="relative border border-yellow-600 mt-1 w-full lg:w-[30%] lg:h-[90%] md:w-[60%] md:h-[92%] sm:w-[80%] flex justify-center items-center">
-          {/* <div className="absolute size-16 z-10 bottom-0.5 right-0 w-8 h-8 md:w-12 md:h-12 animate-pulse duration-1000 infinite">
-            <img src={dots} alt="1 not found " />
-          </div>
-          <div className="absolute z-8 right-6 top-4  w-8 h-8 md:w-12 md:h-12 animate-spin duration-1000 infinite rotate-45">
-            <img src={cube} alt="2 not found " />
-          </div>
-          <div className="absolute z-9 bottom-2 left-7  w-8 h-16 md:w-12 md:h-12 animate-bounce rounded-full duration-1000 infinite">
-            <img src={circle} alt="3 not found " />
-          </div> */}
-          <div className="flex  w-[100%] h-[90%] lg:w-[90%] lg:h-[90%]  md:w-[100%] md:h-[100%] sm:w-[60%] sm:h-[90%] ">
+        <div className="relative mt-1 w-full lg:w-[30%] lg:h-[90%] md:w-[60%] md:h-[92%] sm:w-[80%] flex justify-center items-center">
+          <div className="flex items-center justify-center w-[100%] h-[90%] lg:w-[90%] lg:h-[80%] lg:mt-0 md:w-[100%] md:h-[100%] sm:w-[60%] sm:h-[90%] ">
             <img
-              className="w-[30%] h-[90%] lg:w-[100%] lg:h-[100%] md:w-[100%] md:h-[100%] sm:items-center sm:justify-center sm:mx-auto sm:w-[60%] sm:h-[90%] p-5 md:p-2 transition-all z-[-12] rounded-md animate-scaleInWithGrayscale object-cover duration-5000 shadow-md"
+              className="w-[30%] h-[90%] lg:w-[100%] lg:h-[100%] md:w-[100%] md:h-[100%] sm:items-center sm:justify-center sm:mx-auto sm:w-[60%] sm:h-[90%] p-5 lg:p-9 md:p-2 transition-all z-[-12] rounded-md animate-scaleInWithGrayscale object-cover duration-5000 shadow-lg"
               src={User}
               alt="6 not found "
             />
