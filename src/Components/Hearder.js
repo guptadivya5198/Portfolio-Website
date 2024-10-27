e0077df02c7c92fe37276c4057df2dbd815ac2f7
guptadivya5198 <guptadivya5198@gmail.com>
2024-10-27 23:07:48 +0530

    header

--- src/Components/Hearder.js
+++ src/Components/Hearder.js
@@ -3,8 +3,8 @@ import { FaDochub } from 'react-icons/fa6';
 
 const Hearder = () => {
   return (
-    <div className="w-full bg-gray-50 ">
-      <nav className="h-[3.5rem] w-[100%] sticky flex justify-between items-center sm:px-6 md:px-20 lg:px-28">
+    <div className="w-full bg-gray-50">
+      <nav className="h-[5.1rem] w-[100%] flex justify-between items-center sm:px-6 md:px-20 lg:px-28">
         <div className="flex items-center w-[38%]">
           <FaDochub className="lg:text-4xl md:text-4xl sm:text-xl md:ml-0 sm:ml-0" />
           <p className="ml-0 sm:ml-0 lg:mt-3 sm:text-lg md:text-3xl lg:text-2xl font-semibold">
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -7,7 +7,7 @@ import SendIcon from '@mui/icons-material/Send';
 const HomePage = () => {
   return (
     <section id="Home">
-      <div className="flex flex-col lg:flex-row md:flex-row relative lg:w-[1200px] lg:h-[0px] md:w-[800px] md:h-[240px] sm:w-[300px] sm:h-[300px] items-center justify-center  md:gap-24 lg:my-6 sm:my-12 md:my-12 lg:mx-auto md:mx-auto lg:py-4 lg:px-7 sm:px-6 md:px-6">
+      <div className="flex flex-col lg:flex-row md:flex-row relative lg:w-[80rem] lg:h-[37rem] md:w-[800px] md:h-[240px] sm:w-[300px] sm:h-[300px] items-center justify-center md:gap-24 lg:my-6 sm:my-12 md:my-12 lg:mx-auto md:mx-auto lg:py-4 lg:px-7 sm:px-6 md:px-6">
         {/* left Section  {Text & button}*/}
         <div className="flex flex-col justify-center w-full h-[50%] lg:w-[65%] lg:h-[90%] md:w-[90%] md:h-[93%] sm:w-full sm:h-[50%] lg:gap-6 md:gap-0 sm:gap-8 lg:mt-1 md:mt-2 text-center md:text-left">
           <div className="lg:text-3xl md:text-lg sm:text-3xl font-normal">
@@ -63,7 +63,7 @@ const HomePage = () => {
         <div className="relative mt-1 w-full lg:w-[30%] lg:h-[90%] md:w-[60%] md:h-[92%] sm:w-[80%] flex justify-center items-center">
           <div className="flex items-center mt-3 justify-center w-[100%] h-[90%] lg:w-[90%] lg:h-[80%] lg:mt-0 md:w-[100%] md:h-[100%] sm:w-[60%] sm:h-[90%] ">
             <img
-              className="w-[50%] h-[90%] lg:w-[100%] lg:h-[100%] md:w-[100%] md:h-[100%] sm:items-center sm:justify-center sm:mx-auto sm:w-[60%] sm:h-[100%] p-5 lg:p-9 md:p-2 transition-all z-[-12] rounded-md animate-scaleInWithGrayscale object-cover duration-5000 shadow-lg"
+              className="w-[50%] h-[90%] lg:w-[100%] lg:h-[90%] md:w-[100%] md:h-[100%] sm:items-center sm:justify-center sm:mx-auto sm:w-[60%] sm:h-[100%] p-5 lg:p-9 md:p-2 transition-all z-[-12] rounded-md animate-scaleInWithGrayscale object-cover duration-5000 shadow-lg"
               src={User}
               alt="6 not found "
             />
