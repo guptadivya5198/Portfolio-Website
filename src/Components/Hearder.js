0ac1524f7b51e1714dbcee01d7c6b9e91c7513c3
guptadivya5198 <guptadivya5198@gmail.com>
2024-10-29 00:12:22 +0530

    mesh

--- src/Components/Hearder.js
+++ src/Components/Hearder.js
@@ -5,13 +5,13 @@ const Hearder = () => {
   return (
     <div className="w-full border flex items-center justify-center border-red-400 bg-gray-100">
       <nav className="navbar w-[100%] h-[4.1rem]  flex items-center">
-        <div className="custom-div flex border border-red-400 items-center justify-center w-[34%]">
+        <div className="custom-div flex border border-red-400 items-center justify-center w-[35%]">
           <FaDochub className="lg:text-5xl md:text-3xl text-2xl lg:mt-0 md:mt-2" />
           <p className="lg:text-4xl md:text-2xl text-lg lg:mt-3 md:mt-3 mt-1 lg:font-semibold md:font-semibold font-normal">
             IVYA GUPTA
           </p>
         </div>
-        <div className="border border-red-400 w-[66%]">
+        <div className="border border-red-400 w-[65%]">
           <ul className="flex gap-x-5 mt-1 md:text-xl lg:text-2xl cursor-pointer">
             <li>
               <a href="#Project">Project</a>
