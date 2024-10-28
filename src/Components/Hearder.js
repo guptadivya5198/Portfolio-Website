b4fc182feedce917b2413ac893894582ac981232
guptadivya5198 <guptadivya5198@gmail.com>
2024-10-28 20:34:48 +0530

    mesh

--- src/Components/Hearder.js
+++ src/Components/Hearder.js
@@ -3,16 +3,16 @@ import { FaDochub } from 'react-icons/fa6';
 
 const Hearder = () => {
   return (
-    <div className="w-full bg-gray-50">
-      <nav className="h-[4.6rem] w-[100%] flex justify-between items-center sm:px-6 md:px-20 lg:px-28">
+    <div className="w-full border border-red-400 bg-gray-100">
+      <nav className="navbar w-[100%] h-[4.1rem] max-h-[5rem] flex justify-between items-cepx-6 md:px-20 lg:px-28">
         <div className="flex items-center w-[38%]">
-          <FaDochub className="lg:text-5xl md:text-4xl sm:text-xl md:ml-0 sm:ml-0" />
-          <p className="ml-0 sm:ml-0 lg:mt-5 sm:text-lg md:text-3xl lg:text-3xl font-semibold">
+          <FaDochub className="lg:text-xl md:text-3xl md:ml-0" />
+          <p className="ml-0 lg:text-lg md:text-3xl font-semibold">
             IVYA GUPTA
           </p>
         </div>
         <div className="w-[62%]">
-          <ul className="flex gap-x-6 lg:ml-[50%] md:ml-[30%] xl:ml-[50%] sm:gap-x-5 lg:gap-x-8 sm:text-xs sm:mt-1 md:text-xl lg:text-2xl cursor-pointer">
+          <ul className="flex gap-x-6 lg:ml-[50%] md:ml-[30%] xl:ml-[gap-x-5 lg:gap mt-1 md:text-xl lg:text-2xl cursor-pointer">
             <li>
               <a href="#Project">Project</a>
             </li>
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -7,14 +7,14 @@ import SendIcon from '@mui/icons-material/Send';
 const HomePage = () => {
   return (
     <section id="Home">
-      <div className="flex flex-col lg:flex-row md:flex-row relative lg:w-[80rem] lg:h-[37rem] md:w-[800px] md:h-[240px] sm:w-[300px] sm:h-[300px] items-center justify-center md:gap-24 lg:my-6 sm:my-12 md:my-12 lg:mx-auto md:mx-auto lg:py-4 lg:px-7 sm:px-6 md:px-6">
+      <div className="flex flex-col lg:flex-row md:flex-row relative lg:w-[80rem] lg:h-[37rem] md:w-[800px] md:h-[240px] w-[300px] h-[300px] items-center justify-center md:gap-24 lg:my-6 my-12 md:my-12 lg:mx-auto md:mx-auto lg:py-4 lg:px-7 px-6 md:px-6">
         {/* left Section  {Text & button}*/}
-        <div className="flex flex-col justify-center w-full h-[50%] lg:w-[65%] lg:h-[90%] md:w-[90%] md:h-[93%] sm:w-full sm:h-[50%] lg:gap-6 md:gap-0 sm:gap-8 lg:mt-1 md:mt-2 text-center md:text-left">
-          <div className="lg:text-3xl md:text-lg sm:text-3xl font-normal">
+        <div className="flex flex-col justify-center w-full h-[50%] lg:w-[65%] lg:h-[90%] md:w-[90%] md:h-[93%]  lg:gap-6 md:gap-0 gap-8 lg:mt-1 md:mt-2 text-center md:text-left">
+          <div className="lg:text-3xl md:text-lg text-3xl font-normal">
             Hi!! Divya Gupta
           </div>
           <div>
-            <h2 className="lg:text-3xl md:text-lg sm:text-3xl">
+            <h2 className="lg:text-3xl md:text-lg text-3xl">
               I'm a{' '}
               <span className="text-red-700 hover:scale-90 duration-300">
                 <TypeAnimation
@@ -34,18 +34,16 @@ const HomePage = () => {
                   repeat={Infinity}
                 />
               </span>{' '}
-              <div className="lg:text-3xl md:text-lg sm:text-3xl ">
-                Developer
-              </div>
+              <div className="lg:text-3xl md:text-lg text-3xl ">Developer</div>
             </h2>
           </div>
-          <div className="mt-0 lg:text-xl md:text-base sm:text-base sm:w-[100%] md:w-[100%]">
+          <div className="mt-0 lg:text-xl md:text-base text-base w-[100%] md:w-[100%]">
             I'm a Software Developer and here is my portfolio website. Here
             you'll learn about my journey as a software developer.
           </div>
-          <div className="flex lg:mt-2 md:mt-0 sm:mt-10 lg:mr-[82%] md:items-center md:justify-center sm:items-center">
+          <div className="flex lg:mt-2 md:mt-0 mt-10 lg:mr-[82%] md:items-center md:justify-center items-center">
             <Button
-              className="bg-red-500 mx-auto w-[30%] h-9 lg:w-[100%] lg:h-[90%] md:[40%] sm:w-[26%] sm:h-[90%] xs:w-[30%] xs:h-[50%] lg:px-2 md:px-2 lg:py-2 md:py-2 py-1 mt-3 text-base !shadow-red-200 font-semibold cursor-pointer transition-all duration-75 border-3 border-transparent hover:scale-105"
+              className="bg-red-500 mx-auto w-[30%] h-9 lg:w-[100%] lg:h-[90%] md:[40%] w-[26%] h-[90%] xs:w-[30%] xs:h-[50%] lg:px-2 md:px-2 lg:py-2 md:py-2 py-1 mt-3 text-base !shadow-red-200 font-semibold cursor-pointer transition-all duration-75 border-3 border-transparent hover:scale-105"
               variant="contained"
               SendIcon={<SendIcon />}
               onClick={() =>
@@ -60,10 +58,10 @@ const HomePage = () => {
           </div>
         </div>
         {/* Right Section */}
-        <div className="relative mt-1 w-full lg:w-[30%] lg:h-[90%] md:w-[60%] md:h-[92%] sm:w-[80%] flex justify-center items-center">
-          <div className="flex items-center mt-3 justify-center w-[100%] h-[90%] lg:w-[90%] lg:h-[80%] lg:mt-0 md:w-[100%] md:h-[100%] sm:w-[60%] sm:h-[90%] ">
+        <div className="relative mt-1 w-full lg:w-[30%] lg:h-[90%] md:w-[60%] md:h-[92%] w-[80%] flex justify-center items-center">
+          <div className="flex items-center mt-3 justify-center w-[100%] h-[90%] lg:w-[90%] lg:h-[80%] lg:mt-0 md:w-[100%] md:h-[100%] w-[60%] h-[90%] ">
             <img
-              className="w-[50%] h-[90%] lg:w-[100%] lg:h-[90%] md:w-[100%] md:h-[100%] sm:items-center sm:justify-center sm:mx-auto sm:w-[60%] sm:h-[100%] p-5 lg:p-9 md:p-2 transition-all z-[-12] rounded-md animate-scaleInWithGrayscale object-cover duration-5000 shadow-lg"
+              className="w-[50%] h-[90%] lg:w-[100%] lg:h-[90%] md:w-[100%] md:h-[100%] items-center justify-center mx-auto w-[60%] h-[100%] p-5 lg:p-9 md:p-2 transition-all z-[-12] rounded-md animate-scaleInWithGrayscale object-cover duration-5000 shadow-lg"
               src={User}
               alt="6 not found "
             />
