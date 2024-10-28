26957a5c5949223b1872d0ea700a3fcad53984d2
guptadivya5198 <guptadivya5198@gmail.com>
2024-10-29 01:47:38 +0530

    header

--- src/Components/Hearder.js
+++ src/Components/Hearder.js
@@ -5,7 +5,7 @@ const Hearder = () => {
   return (
     <div className="w-full border flex items-center justify-center border-red-400 bg-gray-100">
       <nav className="navbar w-[100%] h-[4.1rem]  flex items-center">
-        <div className="custom-div border  flex items-center justify-center w-[38%]">
+        <div className="custom-div border rounded-md flex items-center justify-center w-[38%]">
           <FaDochub className="lg:text-5xl border-black md:text-3xl text-2xl lg:mt-0 md:mt-2" />
           <p className="lg:text-4xl md:text-2xl lg:mt-3 md:mt-3 mt-1 lg:font-semibold md:font-semibold font-normal">
             IVYA GUPTA
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -7,14 +7,14 @@ import SendIcon from '@mui/icons-material/Send';
 const HomePage = () => {
   return (
     <section id="Home">
-      <div className="border border-red-400 relative flex lg:flex-row md:flex-row flex-col lg:w-[75rem] lg:h-[32rem] md:w-[800px] md:h-[240px] w-[300px] h-[300px] items-center justify-center md:gap-24 lg:my-6 my-12 md:my-12 lg:mx-auto md:mx-auto lg:py-4 lg:px-7 px-6 md:px-6">
+      <div className="border border-red-400 relative flex lg:flex-row md:flex-row flex-col lg:w-[82%] lg:h-[37rem] md:w-[60rem] md:h-[20rem] w-[30rem] h-[30rem] items-center justify-center md:gap-16 ml-[7rem]">
         {/* left Section  {Text & button}*/}
-        <div className="flex flex-col justify-center w-full h-[50%] lg:w-[65%] lg:h-[90%] md:w-[90%] md:h-[93%]  lg:gap-6 md:gap-0 gap-8 lg:mt-1 md:mt-2 text-center md:text-left">
-          {/* <div className="lg:text-3xl md:text-lg text-3xl font-normal">
+        <div className=" border border-yellow-800 flex flex-col justify-center w-full h-[50%] lg:w-[65%] lg:h-[90%] md:w-[0%] md:h-[93%] lg:gap-6 md:gap-0 gap-8 lg:mt-1 md:mt-2 text-center md:text-left">
+          <div className="lg:text-3xl md:text-lg text-3xl font-normal">
             Hi!! Divya Gupta
-          </div> */}
+          </div>
           <div>
-            {/* <h2 className="lg:text-3xl md:text-lg text-3xl">
+            <h2 className="lg:text-3xl md:text-lg text-3xl">
               I'm a{' '}
               <span className="text-red-700 hover:scale-90 duration-300">
                 <TypeAnimation
@@ -35,13 +35,13 @@ const HomePage = () => {
                 />
               </span>{' '}
               <div className="lg:text-3xl md:text-lg text-3xl ">Developer</div>
-            </h2> */}
+            </h2>
           </div>
           <div className="mt-0 lg:text-xl md:text-base text-base w-[100%] md:w-[100%]">
             I'm a Software Developer and here is my portfolio website. Here
             you'll learn about my journey as a software developer.
           </div>
-          <div className="flex lg:mt-2 md:mt-0 mt-10 lg:mr-[82%] md:items-center md:justify-center items-center">
+          {/* <div className="flex lg:mt-2 md:mt-0 mt-10 lg:mr-[82%] md:items-center md:justify-center items-center">
             <Button
               className="bg-red-500 mx-auto w-[30%] h-9 lg:w-[100%] lg:h-[90%] md:[40%] w-[26%] h-[90%] xs:w-[30%] xs:h-[50%] lg:px-2 md:px-2 lg:py-2 md:py-2 py-1 mt-3 text-base !shadow-red-200 font-semibold cursor-pointer transition-all duration-75 border-3 border-transparent hover:scale-105"
               variant="contained"
@@ -55,13 +55,13 @@ const HomePage = () => {
             >
               Hire Me
             </Button>
-          </div>
+          </div> */}
         </div>
         {/* Right Section */}
-        <div className="relative mt-1 w-full lg:w-[30%] lg:h-[90%] md:w-[60%] md:h-[92%] w-[80%] flex justify-center items-center">
-          <div className="flex items-center mt-3 justify-center w-[100%] h-[90%] lg:w-[90%] lg:h-[80%] lg:mt-0 md:w-[100%] md:h-[100%] w-[60%] h-[90%] ">
+        <div className="border border-yellow-800 relative lg:w-[30%] lg:h-[90%] md:w-[60%] md:h-[92%] w-[80%] flex justify-center items-center">
+          <div className="flex items-center mt-3 justify-center w-[100%] h-[90%] lg:w-[90%] lg:h-[80%] md:w-[100%] md:h-[100%] ">
             <img
-              className="w-[50%] h-[90%] lg:w-[100%] lg:h-[90%] md:w-[100%] md:h-[100%] items-center justify-center mx-auto w-[60%] h-[100%] p-5 lg:p-9 md:p-2 transition-all z-[-12] rounded-md animate-scaleInWithGrayscale object-cover duration-5000 shadow-lg"
+              className="w-[50%] h-[90%] lg:w-[100%] lg:h-[90%] md:w-[100%] md:h-[100%] items-center justify-center z-[-12] rounded-md animate-scaleInWithGrayscale object-cover duration-5000 shadow-lg"
               src={User}
               alt="6 not found "
             />
