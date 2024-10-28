1990689d7d793f9d6b083948a97b910713606061
guptadivya5198 <guptadivya5198@gmail.com>
2024-10-28 23:08:00 +0530

    mesh

--- src/Components/Hearder.js
+++ src/Components/Hearder.js
@@ -3,16 +3,16 @@ import { FaDochub } from 'react-icons/fa6';
 
 const Hearder = () => {
   return (
-    <div className="w-full border border-red-400 bg-gray-100">
+    <div className="w-full border flex items-center justify-center border-red-400 bg-gray-100">
       <nav className="navbar w-[100%] h-[4.1rem] max-h-[4rem] flex items-center">
-        <div className="flex border border-red-400  px-[4%] md:px-[6%] lg:px-[3%] items-center w-[40%]">
-          <FaDochub className="lg:text-5xl md:text-3xl text-2xl lg:ml-0 md:ml-0 ml-[0%] lg:mt-0 md:mt-2 mt-2" />
-          <p className="lg:text-4xl md:text-2xl text-lg lg:ml-0 md:ml-0 ml-0 lg:mt-3 md:mt-3 mt-3 lg:font-semibold md:font-semibold font-normal">
+        <div className="custom-div flex border border-red-400 items-center w-[34%]">
+          <FaDochub className="lg:text-5xl md:text-3xl text-2xl lg:mt-0 md:mt-2 mt-2" />
+          <p className="lg:text-4xl md:text-2xl text-lg lg:mt-3 md:mt-3 mt-3 lg:font-semibold md:font-semibold font-normal">
             IVYA GUPTA
           </p>
         </div>
-        <div className="border border-red-400 w-[60%]">
-          <ul className="flex gap-x-6 lg:ml-[50%] md:ml-[30%] xl:ml-[gap-x-5 lg:gap mt-1 md:text-xl lg:text-2xl cursor-pointer">
+        <div className="border border-red-400 w-[66%]">
+          <ul className="flex gap-x-5 mt-1 md:text-xl lg:text-2xl cursor-pointer">
             <li>
               <a href="#Project">Project</a>
             </li>
